a6d54af270e327da5d05f9703ab9cc4c27baab94
Anmol Sharma <anmolsharma009a@gmail.com>
2022-08-03 19:57:27 +0530

    handle-cover.php code improved

--- handle_cover.php
+++ handle_cover.php
@@ -3,24 +3,37 @@ require_once "partials/_dbconnect.php";
 require_once "functions.php";
 
 
+// update covers
+$stmt12  = $conn->prepare("UPDATE covers SET `cover_path` = ? WHERE `w_id` = ?");
+$stmt12->bind_param("si", $img_path, $webtoon_id);
+
+
 $sql = "SELECT * FROM `cover_details`";
 $result = mysqli_query($conn, $sql);
 while ($row = mysqli_fetch_assoc($result)) {
     $webtoon_id = $row['w_id'];
-    $webtoon_title = $row['w_id'];
+    $webtoon_title = $row['w_title'];
     $cover_url = $row['cover_url'];
     $cover_path = $row['cover_path'];
+    $img_ext = '/\.(jpg|jpeg|png|webp)/';
+    preg_match($img_ext, $cover_url, $matches);
+    $img_ext = $matches[0];
+    $img_path = "img/" . $webtoon_id . $img_ext;
+    
+    echo "<hr>";
+    echo "Webtoon ID : $webtoon_id<br>";
+    echo "Webtoon Title : $webtoon_title<br>";
+    echo "Cover Url : $cover_url<br>";
+    echo "Cover Path : $img_path<br>";
 
-    if (!$cover_path) {
-        $img_ext = '/.[\w]{3,4}/';
-        $img_ext = preg_match($img_ext, $cover_url, $matches);
-        $cover_path = "img/" . $webtoon_title . $img_ext;
-        save_img($cover, $webtoon_title, $cover_path);
+    if ($cover_path !== $img_path) {
+        save_img($cover_url, $webtoon_title, $img_path);
+        // update cover details
+        $result2 = $stmt12->execute();
+        // if ($result2) {
+        //     echo "Updated cover path";
+        //     echo "<br>";
+        // }
     }
 
-    echo "<hr>";
-    echo "Webtoon ID : $webtoon_title<br>";
-    echo "Webtoon Title : $webtoon_title<br>";
-    echo "Cover Url : $webtoon_title<br>";
-    echo "Cover Path : $webtoon_title<br>";
 }
