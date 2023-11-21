76c8ebb99c7a5202ba595080f0fd020c58e3fc47
Anmol Sharma <anmolsharma009a@gmail.com>
2023-11-21 22:59:31 +0530

    mv component to components

--- component/chapter.php
+++
@@ -1,5 +0,0 @@
-<li class="chapter list-group-item">
-    <a href="<?=$chapter['url']?>"  target="blank" class="text-dark text-decoration-none">
-        Chapter <?=(int)$chapter['number'] == $chapter['number']?(int)$chapter['number']:$chapter['number']?>
-    </a>
-</li>
--- component/webtoon_img.php
+++
@@ -1,3 +0,0 @@
-<a href="<?=$webtoon['url']?>">
-    <img src="<?=$webtoon['cover_url']?>" alt="<?=$webtoon['title']?>">
-</a>
